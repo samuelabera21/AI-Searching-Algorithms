1d0a2a78b80cb4c5047c97a7b1b96fe26edeb456
Samuel Abera <sami21.good.bad@gmail.com>
2026-04-06 10:36:23 +0300

    update the Heuristic

--- heuristic.py
+++ heuristic.py
@@ -1,9 +1,13 @@
 heuristic = {
-    "Debre Birhan": 600,
-    "Addis Ababa": 500,
-    "Debre Markos": 265,
-    "Dessie": 700,
-    "Kombolcha": 650,
+    "Debre Birhan": 540,
+    "Addis Ababa": 430,
+    "Adama": 470,
+    "Shewa Robit": 360,
+    "Finote Selam": 120,
+    "Debre Markos": 260,
+    "Injibara": 70,
+    "Dessie": 210,
+    "Kombolcha": 260,
     "Woldiya": 360,
     "Bahir Dar": 0
 }
